2a2aae70e078c130c69425d187701f84020d1692
Sergio Ismael <166397985+sergiocayuqueo@users.noreply.github.com>
2026-08-05 16:47:39 -0400

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+      const lines = document.querySelectorAll('.line');
+        const observerOptions = { 
+            threshold: 0.1,
+            rootMargin: "0px 0px -10% 0px" 
+        };
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('active');
+                }
+            });
+        }, observerOptions);
+
+        lines.forEach(line => observer.observe(line));
